4dde7e5a08b25734b501207372f11fd09464e236
MingYang-Hydrate <71815635+MingYang-Hydrate@users.noreply.github.com>
2020-11-10 16:25:53 +0800

    Update PS2_1.R
    Incomplete work for 1.4

--- Assignment2/PS2_1.R
+++ Assignment2/PS2_1.R
@@ -7,6 +7,12 @@ select(Sig_Eqs, YEAR, COUNTRY, DEATHS)  %>%
   group_by(COUNTRY) %>%
   summarise(countrytotal=sum(DEATHS, na.rm = T)) %>%
   arrange(desc(countrytotal))
+# @MingYANG recommended:
+# there are some differences with TOTEL_DEATHS and DEATHS
+# using "select" for calculating is ok,but you should print the top 10 countries 
+# adding "TOPTEN<-" in the beginning of line 6 to save the selected data and
+# using "print(TOPTEN[1:10,])" for printing the top 10 countries
+# the end
 
 #1.3
 select(Sig_Eqs, YEAR, EQ_PRIMARY) %>%
@@ -20,6 +26,7 @@ select(Sig_Eqs, YEAR, EQ_PRIMARY) %>%
   count() %>%
   ggplot(aes(x=YEAR, y=n)) + 
   geom_point()
+# good work
 
 NUM <- Sig_Eqs%>%
 select(YEAR, EQ_PRIMARY) %>%
@@ -60,5 +67,21 @@ CountEq_LargestEq <- function(states){
 }
 CountEq_LargestEq("CHINA")
  
-
-
+# @MingYANG noticed
+# Search the biggest earthquake for each country, you could take the following code as a reference:
+# country<-unique(Sig_Eqs$COUNTRY)
+# EQ_number<-c()
+# EQ_country<-c()
+# EQmax_date<-c()
+# for(i in country){
+#   a<-as.numeric(CountEq_LargestEq(i)[1])
+#   b<-i
+#   c<-as.character(CountEq_LargestEq(i)[2])
+#   EQ_number <-c(EQ_number,a)
+#   EQ_country<-c(EQ_country,b)
+#   EQmax_date<-c(EQmax_date,c)
+# }
+# df1<-data.frame(EQ_country,EQ_number ,EQmax_date)
+# tbl_new<-as_tibble(df1)
+# tbl_new %>% 
+#   arrange(desc(EQ_number))
